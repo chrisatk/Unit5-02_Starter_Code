c5f2d29f8316d4212dcc8c7785e8e2e84ab6d5f0
Chris Atkinson <46910324+chrisatk@users.noreply.github.com>
2023-05-05 10:20:57 -0400

    Create Q3WordTriangle.py

---
+++ Q3WordTriangle.py
@@ -0,0 +1,7 @@
+# Create a program that will prompt the user for a word, and return a 'word triangle'. For example, if the user types in the word "PYTHON", your program will output:
+# P
+# PY
+# PYT
+# PYTH
+# PYTHO
+# PYTHON
